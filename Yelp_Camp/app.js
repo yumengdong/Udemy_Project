d19a3300b549936db00ac6db5a14fdb206dac4d9
Yumeng DONG <ymbravado@gmail.com>
2019-11-20 07:19:48 -0800

    Add start code in package.json

--- Yelp_Camp/app.js
+++ Yelp_Camp/app.js
@@ -86,3 +86,5 @@ app.use('/campgrounds/:id/comments', commentRoutes);
 app.listen(3000, function(){
     console.log('The YelpCamp Server Has Started!')
 })
+
+app.listen(process.env.PORT, process.env.IP);
