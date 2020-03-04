81c959cdefad643e19c2242e5171226e1637e51b
Yumeng DONG <ymbravado@gmail.com>
2020-03-03 20:07:58 -0800

    Update header and footer ejs files in partials folder

--- Napa_Wine_Project/routes/index.js
+++ Napa_Wine_Project/routes/index.js
@@ -28,7 +28,7 @@ router.post('/register', function(req, res){
             return res.redirect('/register');
         }
         passport.authenticate('local')(req, res, function(){
-            req.flash('success', 'Welcome to YelpCamp ' + user.username);
+            req.flash('success', 'Welcome to Napa Wineries ' + user.username);
             res.redirect('/campgrounds');
          })
     })
