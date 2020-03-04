28cc4be780486f5fdd9c19b3473e44282ff5a42e
Yumeng DONG <ymbravado@gmail.com>
2020-03-03 23:08:38 -0800

    Update mongdb in app.js and show.ejs

--- Napa_Wine_Project/app.js
+++ Napa_Wine_Project/app.js
@@ -43,9 +43,10 @@ mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 
 // console.log(process.env.DATABASEULR);
-// mongoose.connect('mongodb://localhost/yelp_camp');
-var url = process.env.DATABASEULR || "mongodb://localhost/yelp_camp"
-mongoose.connect(url);
+mongoose.connect('mongodb://localhost/napa_wine');
+
+// var url = process.env.DATABASEULR || "mongodb://localhost/yelp_camp"
+// mongoose.connect(url);
 
 // mongoose.connect('mongodb+srv://dongyumeng88:password123321@cluster0-p6ynm.mongodb.net/yelp_camp?retryWrites=true&w=majority', {
 //     useNewUrlParser: true,
